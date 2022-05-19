52b64b3c6962b941b879b60ad73aee6d7db95ce3
Sophia Hyosun Kim <hyosunkim1019@gmail.com>
2022-05-19 12:41:21 +0900

    Add files via upload

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+import java.util.ArrayList;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
